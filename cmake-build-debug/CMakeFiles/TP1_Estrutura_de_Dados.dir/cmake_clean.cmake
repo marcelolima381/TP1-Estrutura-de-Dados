f805212f992d6a962fcd52d583c280f6c51c0a58
Marcelo Medeiros de Lima <marcelolima381@gmail.com>
2019-04-11 23:22:16 -0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/TP1_Estrutura_de_Dados.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_Estrutura_de_Dados.dir/main.c.o"
+  "TP1_Estrutura_de_Dados.pdb"
+  "TP1_Estrutura_de_Dados"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP1_Estrutura_de_Dados.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TP1_Estrutura_de_Dados.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TP1_Estrutura_de_Dados.
+# This may be replaced when dependencies are built.
